85ed8f75e68b665e45df7ecefee2aa5a7f39fc72
Vamba45 <vambafool@bk.ru>
2023-12-30 15:04:53 +0300

    add customer-reducer to store

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
 
 export default function Application () {
     const dispatch = useDispatch();
     const cash = useSelector(state => state.cash.cash);
+    const customers = useSelector(state => state.customers.customers)
 
     const addCash = (cash) => {
         dispatch({type: "ADD_CASH", payload: cash})
@@ -13,11 +15,40 @@ export default function Application () {
         dispatch({type: "GET_CASH", payload: cash})
     }
 
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: "id:" + name 
+        }
+
+        dispatch(addCustomerAction(customer))
+    }
+
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id))
+    }
+
     return (
         <div className="App">
             <div>{cash}</div>
             <button onClick={() => getCash(Number(prompt()))}>Снять со счёта</button>
             <button onClick={() => addCash(Number(prompt()))}>Положить на счёт</button>
+            <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+            <button onClick={() => removeCustomer(prompt())}>Удалить клиента</button>
+            {customers.length > 0 ?
+                <div>
+                    {
+                        customers.map(customer => 
+                            <div onClick={() => removeCustomer(customer)}>
+                                {customer.name}
+                            </div>
+                        )
+                    }
+                </div> :
+                <div>
+                    Клиенты отсутствуют
+                </div>
+            }
         </div>
     )
 }
