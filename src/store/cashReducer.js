7a863e279867a9d03e21506b72ba028ecdf2de7e
Vamba45 <vambafool@bk.ru>
2024-01-05 15:23:22 +0300

    add redux-saga

--- src/store/cashReducer.js
+++ src/store/cashReducer.js
@@ -10,7 +10,7 @@ export const cashReducer = (state = defaultState, action) => {
         case "GET_CASH":
             return {...state, cash: state.cash - action.payload}
 
-            default:
-                return state
+        default:
+            return state
     }
 }
